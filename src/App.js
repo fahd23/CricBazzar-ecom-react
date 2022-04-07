8dfcf654cfe724ec83a8e99b677925c0bc7b11fa
fahd23 <fahdpno@gmail.com>
2022-04-07 11:53:49 +0530

    REFACTOR: import file in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "App.css";
 import Mockman from "mockman-js";
 import { Routes, Route } from "react-router-dom";
-import Home from "pages/home";
-import Products from "pages/product";
-import Carts from "pages/cart";
-import LoginPage from "pages/auth/login";
-import SignupPage from "pages/auth/signup";
-import ForgetPassPage from "pages/auth/forgotpass";
-import WishListPage from "pages/wishlist";
-import NotFound from "pages/404/404";
+import {
+  Home,
+  Products,
+  Carts,
+  LoginPage,
+  SignupPage,
+  ForgetPassPage,
+  WishListPage,
+  NotFound,
+} from "pages";
 
 function App() {
   return (
